4e7cbfe4c668ee4d0e5bc55580cdca773e3c2225
jinyi <jinyi@vt.edu>
2022-10-22 00:34:54 -0400

    1

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1500,
   },
-  base: env.VITE_MODE === 'production' ? './' : '/',
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   })
