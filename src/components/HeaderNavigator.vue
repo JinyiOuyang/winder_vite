13b390890800c4ebd9548cdfd9850e7a6c2adaf9
jinyi <jinyi@vt.edu>
2022-12-03 20:23:42 -0500

    New

--- src/components/HeaderNavigator.vue
+++ src/components/HeaderNavigator.vue
@@ -15,13 +15,19 @@
 <script>
 import Header from "./Header.vue";
 
-const setLink = (link) => {
-  localStorage.setItem('link', link)
-}
 export default {
   name: "HeaderNavigator",
   components: {Header},
+  methods: {
+    setLink(link) {
+      localStorage.setItem('link', link)
+      console.log(localStorage.getItem("link"))
+      console.log(window.location.href)
+      // window.location.reload()
 
+
+    }
+  }
 }
 
 </script>
@@ -29,10 +35,6 @@ export default {
 <script setup>
 import {reactive, ref} from "vue";
 const user = ref(localStorage.getItem("userid"))
-
-const setLink = (link) => {
-  localStorage.setItem('link', link)
-}
 </script>
 
 
